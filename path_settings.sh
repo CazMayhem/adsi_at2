0e3be742788e5f0d553f0ed621204f1d3d9a347b
CazMayhem <paipac@192-168-1-109.tpgi.com.au>
2022-03-23 21:19:23 +1100

    pytorch multi-class

---
+++ path_settings.sh
@@ -0,0 +1,17 @@
+
+export PATH=${PATH}:/Library/Frameworks/Python.framework/Versions/3.10/bin
+export PATH=${PATH}:/usr/local/opt
+export PATH=${PATH}:/Library/Developer/CommandLineTools/usr/bin
+
+export PYTHONPATH=${PATH}:/Library/Frameworks/Python.framework/Versions/3.10/bin
+export PYTHONPATH=${PATH}:/usr/local/opt
+export PYTHONPATH=${PATH}:/Library/Developer/CommandLineTools/usr/bin
+
+export PYTHONPATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PYTHONPATH=${PYTHONPATH}:/Library/Developer/CommandLineTools/usr/bin
+export PYTHONPATH=${PYTHONPATH}:/Users/paipac/Library/Python/3.8/bin
+
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=${PATH}:/Library/Developer/CommandLineTools/usr/bin
+export PATH=${PATH}:/Users/paipac/Library/Python/3.8/bin
+
